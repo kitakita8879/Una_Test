2f988e73a14c9b584e123f782894b7fa26a55b5d
Una <unaliu@horizon-brands.com>
2024-04-15 16:14:13 +0800

    [ListView+RecyclerView] 優化代碼 1. ListView 改以BaseAdapter呈現

--- app/src/main/java/com/example/una_test/ListViewAndRecycleViewActivity.java
+++ app/src/main/java/com/example/una_test/ListViewAndRecycleViewActivity.java
@@ -7,7 +7,7 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -30,17 +30,67 @@ public class ListViewAndRecycleViewActivity extends AppCompatActivity {
             mData.add(i);
         }
 
-        // TODO: 2024/4/15 adapter 相關用法
+        //ListView
         ListView lvShow = findViewById(R.id.list_view);
-        ArrayAdapter<Integer> listAdapter = new ArrayAdapter<>(this, R.layout.test_1_view_item, mData);
-        lvShow.setAdapter(listAdapter);
+        lvShow.setAdapter(new ListViewAdapter(mData));
 
+        //RecyclerView
         RecyclerView rvShow = findViewById(R.id.recycler_view);
         rvShow.setLayoutManager(new LinearLayoutManager(this));
         rvShow.setAdapter(new RecyclerViewAdapter(mData));
 
         RecyclerSpace decoration = new RecyclerSpace((int) RecyclerSpace.convertDpToPixel(15, this));
         rvShow.addItemDecoration(decoration);
+    }
+
+}
+
+class ListViewAdapter extends BaseAdapter {
+    private final ArrayList<Integer> mData;
+
+    public ListViewAdapter(ArrayList<Integer> data) {
+        this.mData = data;
+    }
+
+    static class ViewHolder {
+        TextView txtLvItem;
+
+        ViewHolder(View item) {
+            txtLvItem = item.findViewById(R.id.list_view_item);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.test_1_view_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Integer data = mData.get(position);
+        holder.txtLvItem.setText(String.valueOf(data));
+
+        return convertView;
     }
 
 }
