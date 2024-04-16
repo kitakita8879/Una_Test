9c303fda28c3c8efd54931aac23414c05cd8b391
Una <unaliu@horizon-brands.com>
2024-04-16 17:58:47 +0800

    [Una Test 2] 新增Asset 1. 新增drawable 資源

---
+++ app/src/main/java/com/example/una_test/ChargingMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.una_test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChargingMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charging_main_page);
+
+
+    }
+}
