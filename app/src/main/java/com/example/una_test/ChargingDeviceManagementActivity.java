a8fc9ca9f19fda3516e28ed7cf617a12dd1abbbc
Una <unaliu@horizon-brands.com>
2024-04-23 09:04:55 +0800

    [Una Test 2] 新增功能 1. 開啟data binding 2. 新增add 按鈕

--- app/src/main/java/com/example/una_test/ChargingDeviceManagementActivity.java
+++ app/src/main/java/com/example/una_test/ChargingDeviceManagementActivity.java
@@ -3,7 +3,6 @@ package com.example.una_test;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -16,30 +15,38 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.una_test.databinding.ActivityChargingDeviceManagementBinding;
+import com.example.una_test.databinding.Test2AddDialogBinding;
+import com.example.una_test.databinding.Test2ViewItemBinding;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class ChargingDeviceManagementActivity extends AppCompatActivity {
-    private final ArrayList<Data> mData = new ArrayList<>();
+    public final ArrayList<Data> dataList = new ArrayList<>();
     private ImageView imgCheck1, imgCheck2;
     private Dialog mDialogRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_charging_device_management);
+        ActivityChargingDeviceManagementBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_charging_device_management);
 
-        mData.add(new Data(1, 12345678, true));
-        mData.add(new Data(2, 87654321, false));
+        dataList.add(new Data(1, 12345678, true));
+        dataList.add(new Data(2, 87654321, false));
 
         final Boolean[] isRemoveData = {false};
-        findViewById(R.id.img_back).setOnClickListener(v -> {
+        binding.imgBack.setOnClickListener(v -> {
             if (isRemoveData[0]) {
                 Intent intent = new Intent();
-                intent.putExtra("name", mData.get(0).name);
+                intent.putExtra("name", dataList.get(0).name);
                 setResult(RESULT_OK, intent);
             }
             finish();
@@ -54,27 +61,50 @@ public class ChargingDeviceManagementActivity extends AppCompatActivity {
         TextView txtCancel = mViewDialog.findViewById(R.id.txt_cancel);
         TextView txtRemove = mViewDialog.findViewById(R.id.txt_remove);
 
-        RecyclerView rvShow = findViewById(R.id.recycler_view_test2);
-        rvShow.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerViewAdapter rvAdapter = new RecyclerViewAdapter(mData);
+        binding.recyclerViewTest2.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewAdapter rvAdapter = new RecyclerViewAdapter(dataList);
         rvAdapter.setOnRecyclerItemClickListener((view, position) -> {
             checkRemoveMode(true);
             txtRemove.setOnClickListener(v -> {
-                mData.remove(position);
+                dataList.remove(position);
                 rvAdapter.notifyItemRemoved(position);
                 mDialogRemove.dismiss();
                 isRemoveData[0] = true;
             });
         });
-        rvShow.setAdapter(rvAdapter);
+        binding.recyclerViewTest2.setAdapter(rvAdapter);
 
         ChargingDeviceManagementActivity.RecyclerSpace decoration = new
                 ChargingDeviceManagementActivity.RecyclerSpace(convertDpToPixel(30, this));
-        rvShow.addItemDecoration(decoration);
+        binding.recyclerViewTest2.addItemDecoration(decoration);
 
         txtCancel.setOnClickListener(v -> mDialogRemove.dismiss());
         imgCheck1.setOnClickListener(v -> checkRemoveMode(true));
         imgCheck2.setOnClickListener(v -> checkRemoveMode(false));
+
+        binding.viewAdd.setOnClickListener(v -> showAddDialog());
+    }
+
+    private void showAddDialog() {
+        Dialog addDialog = new Dialog(ChargingDeviceManagementActivity.this,
+                R.style.dialogRemove);
+        View viewDialog = View.inflate(ChargingDeviceManagementActivity.this,
+                R.layout.test_2_add_dialog, null);
+        Test2AddDialogBinding addDialogBinding = Test2AddDialogBinding.bind(viewDialog);
+        addDialog.setContentView(viewDialog);
+        addDialog.show();
+        Objects.requireNonNull(addDialog.getWindow()).setLayout(
+                convertDpToPixel(300, ChargingDeviceManagementActivity.this),
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        addDialogBinding.txtAdd.setOnClickListener(v -> {
+            if (!addDialogBinding.editNum.getText().toString().isEmpty()) {
+                int name = Integer.parseInt(addDialogBinding.editNum.getText().toString());
+                int devId = name + 10000000;
+                dataList.add(new Data(name, devId, false));
+                addDialog.dismiss();
+            }
+        });
+        addDialogBinding.txtCancel.setOnClickListener(v -> addDialog.dismiss());
     }
 
     private void checkRemoveMode(boolean checkMode) {
@@ -87,86 +117,79 @@ public class ChargingDeviceManagementActivity extends AppCompatActivity {
         }
     }
 
-    private static class Data {
-        int name, devId;
-        boolean fwUpdate;
+    public static class Data extends BaseObservable {
+        public int name, devId;
+        @Bindable
+        public boolean fwUpdate;
 
         Data(int name, int devId, boolean fwUpdate) {
             this.name = name;
             this.devId = devId;
             this.fwUpdate = fwUpdate;
         }
+
+        @Bindable
+        public boolean getFwUpdate(){
+            return this.fwUpdate;
+        }
+
+        public void setFwUpdate(boolean fwUpdate) {
+            this.fwUpdate = fwUpdate;
+            notifyPropertyChanged(BR.fwUpdate);
+        }
     }
 
     private class RecyclerViewAdapter extends RecyclerView.Adapter<ChargingDeviceManagementActivity
             .RecyclerViewAdapter.ViewHolder> {
         private final ArrayList<Data> mDataList;
-        private ChargingDeviceManagementActivity.OnRecyclerItemClickListener mClickListener;
+        private OnRecyclerItemClickListener mClickListener;
 
         public RecyclerViewAdapter(ArrayList<Data> dataList) {
             this.mDataList = dataList;
         }
 
-        public void setOnRecyclerItemClickListener(ChargingDeviceManagementActivity
-                                                           .OnRecyclerItemClickListener listener) {
+        public void setOnRecyclerItemClickListener(OnRecyclerItemClickListener listener) {
             this.mClickListener = listener;
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
-            final TextView txtName, txtDevID, txtVersion, txtFwUpdate;
-            final ImageView imgDel, imgError;
-            final View viewItem;
+            final Test2ViewItemBinding binding;
 
             ViewHolder(View item) {
                 super(item);
-                txtName = item.findViewById(R.id.txt_name);
-                txtDevID = item.findViewById(R.id.txt_dev_id);
-                txtVersion = item.findViewById(R.id.txt_version);
-                txtFwUpdate = item.findViewById(R.id.txt_fw_update);
-                imgDel = item.findViewById(R.id.img_del);
-                imgError = item.findViewById(R.id.img_error);
-                viewItem = item.findViewById(R.id.view_item);
+                binding = Test2ViewItemBinding.bind(item);
             }
         }
 
         @NonNull
         @Override
-        public ChargingDeviceManagementActivity.RecyclerViewAdapter.ViewHolder
-        onCreateViewHolder(ViewGroup parent, int viewType) {
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View itemView = LayoutInflater.from(parent.getContext()).inflate(R
                     .layout.test_2_view_item, parent, false);
-            return new ChargingDeviceManagementActivity.RecyclerViewAdapter.ViewHolder(itemView);
+            return new ViewHolder(itemView);
         }
 
         @Override
         public void onBindViewHolder(ChargingDeviceManagementActivity.RecyclerViewAdapter
                                              .ViewHolder holder, int position) {
-            int data = mDataList.get(position).name;
-            int id = mDataList.get(position).devId;
-            boolean fw = mDataList.get(position).fwUpdate;
-            holder.txtName.setText(String.format(getString(R.string.txt_name), data));
-            holder.txtDevID.setText(String.format(getString(R.string.txt_dev_id), id));
-            if (fw) {
-                holder.txtFwUpdate.setText(String.format(getString(R.string.txt_fw_need_update)));
-                holder.txtFwUpdate.setTextColor(Color.parseColor("#0094D6"));
-                holder.imgError.setVisibility(View.VISIBLE);
-            } else {
-                holder.txtFwUpdate.setText(String.format(getString(R.string.txt_fw_not_need_update)));
-                holder.txtFwUpdate.setTextColor(Color.parseColor("#757575"));
-                holder.imgError.setVisibility(View.GONE);
-            }
-            holder.imgDel.setOnClickListener(v -> {
+            holder.binding.setData(mDataList.get(position));
+            holder.binding.imgDel.setOnClickListener(v -> {
                 mClickListener.onRecyclerItemClick(v, holder.getAdapterPosition());
                 mDialogRemove.show();
                 Objects.requireNonNull(mDialogRemove.getWindow()).setLayout(
                         convertDpToPixel(300, ChargingDeviceManagementActivity.this),
                         WindowManager.LayoutParams.WRAP_CONTENT);
             });
-            holder.viewItem.setOnClickListener(v -> {
+            holder.binding.viewItem.setOnClickListener(v -> {
                 Intent intent = new Intent();
                 intent.putExtra("name", mDataList.get(holder.getAdapterPosition()).name);
                 setResult(RESULT_OK, intent);
                 finish();
+            });
+            holder.binding.imgDevice.setOnClickListener(v -> {
+                boolean fwReverse = mDataList.get(holder.getAdapterPosition()).fwUpdate;
+                mDataList.get(holder.getAdapterPosition()).setFwUpdate(!fwReverse);
+                notifyItemChanged(holder.getAdapterPosition());
             });
         }
 
