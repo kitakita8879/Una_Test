e69d439a5fd3f65f314ec6747834edfd790c3e0c
Una <unaliu@horizon-brands.com>
2024-04-23 09:03:19 +0800

    [Una Test 2] 優化代碼、新增assets

--- app/src/main/java/com/example/una_test/ChargingMainActivity.java
+++ app/src/main/java/com/example/una_test/ChargingMainActivity.java
@@ -26,9 +26,9 @@ public class ChargingMainActivity extends AppCompatActivity {
     private final int colorBlue = Color.parseColor("#0094D6");
     private int mMode = 1;
     private int mNameData = 1;
-    private ImageView imgMode1, imgMode2, imgMode3;
-    private TextView txtMode1, txtMode2, txtMode3;
-    private Group groupMode1, groupMode2, groupMode3;
+    private ImageView mImgMode1, mImgMode2, mImgMode3;
+    private TextView mTxtMode1, mTxtMode2, mTxtMode3;
+    private Group mGroupMode1, mGroupMode2, mGroupMode3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +40,9 @@ public class ChargingMainActivity extends AppCompatActivity {
         Group groupUnlock = findViewById(R.id.group_unlock);
 
         View modeSelectBtn = findViewById(R.id.view_mode_select_bg);
-
-        Dialog mDialogMode = new Dialog(ChargingMainActivity.this, R.style.dialogMode);
-        View mViewDialog = View.inflate(ChargingMainActivity.this,
-                R.layout.test_2_mode_select_dialog, null);
-        mDialogMode.setContentView(mViewDialog);
-
-        Space mode1 = mViewDialog.findViewById(R.id.space_mode_1);
-        Space mode2 = mViewDialog.findViewById(R.id.space_mode_2);
-        Space mode3 = mViewDialog.findViewById(R.id.space_mode_3);
-        imgMode1 = mViewDialog.findViewById(R.id.img_mode_1);
-        imgMode2 = mViewDialog.findViewById(R.id.img_mode_2);
-        imgMode3 = mViewDialog.findViewById(R.id.img_mode_3);
-        txtMode1 = mViewDialog.findViewById(R.id.txt_mode_1);
-        txtMode2 = mViewDialog.findViewById(R.id.txt_mode_2);
-        txtMode3 = mViewDialog.findViewById(R.id.txt_mode_3);
-        groupMode1 = findViewById(R.id.group_mode_realtime);
-        groupMode2 = findViewById(R.id.group_mode_quantitative);
-        groupMode3 = findViewById(R.id.group_mode_fixed_time);
+        mGroupMode1 = findViewById(R.id.group_mode_realtime);
+        mGroupMode2 = findViewById(R.id.group_mode_quantitative);
+        mGroupMode3 = findViewById(R.id.group_mode_fixed_time);
 
         ImageView imgBarItemEV = findViewById(R.id.img_bar_item_ev);
 
@@ -77,30 +62,8 @@ public class ChargingMainActivity extends AppCompatActivity {
         });
 
         modeSelectBtn.setOnClickListener(v -> {
+            showModeDialog();
             modeSelect(mMode);
-            mDialogMode.show();
-            Objects.requireNonNull(mDialogMode.getWindow()).setGravity(Gravity.BOTTOM);
-            float height = 235 * ((float) ChargingMainActivity.this.getResources()
-                    .getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-            mDialogMode.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, (int) height);
-        });
-
-        mode1.setOnClickListener(v -> {
-            mMode = 1;
-            modeSelect(mMode);
-            mDialogMode.dismiss();
-        });
-
-        mode2.setOnClickListener(v -> {
-            mMode = 2;
-            modeSelect(mMode);
-            mDialogMode.dismiss();
-        });
-
-        mode3.setOnClickListener(v -> {
-            mMode = 3;
-            modeSelect(mMode);
-            mDialogMode.dismiss();
         });
 
         ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
@@ -121,40 +84,81 @@ public class ChargingMainActivity extends AppCompatActivity {
         });
     }
 
+    private void showModeDialog(){
+        Dialog modeDialog = new Dialog(ChargingMainActivity.this, R.style.dialogMode);
+        View viewDialog = View.inflate(ChargingMainActivity.this,
+                R.layout.test_2_mode_select_dialog, null);
+        modeDialog.setContentView(viewDialog);
+        Space mode1 = viewDialog.findViewById(R.id.space_mode_1);
+        Space mode2 = viewDialog.findViewById(R.id.space_mode_2);
+        Space mode3 = viewDialog.findViewById(R.id.space_mode_3);
+        mImgMode1 = viewDialog.findViewById(R.id.img_mode_1);
+        mImgMode2 = viewDialog.findViewById(R.id.img_mode_2);
+        mImgMode3 = viewDialog.findViewById(R.id.img_mode_3);
+        mTxtMode1 = viewDialog.findViewById(R.id.txt_mode_1);
+        mTxtMode2 = viewDialog.findViewById(R.id.txt_mode_2);
+        mTxtMode3 = viewDialog.findViewById(R.id.txt_mode_3);
+
+        modeDialog.show();
+        Objects.requireNonNull(modeDialog.getWindow()).setGravity(Gravity.BOTTOM);
+        float height = 235 * ((float) ChargingMainActivity.this.getResources()
+                .getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        modeDialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, (int) height);
+
+        mode1.setOnClickListener(v -> {
+            mMode = 1;
+            modeSelect(mMode);
+            modeDialog.dismiss();
+        });
+
+        mode2.setOnClickListener(v -> {
+            mMode = 2;
+            modeSelect(mMode);
+            modeDialog.dismiss();
+        });
+
+        mode3.setOnClickListener(v -> {
+            mMode = 3;
+            modeSelect(mMode);
+            modeDialog.dismiss();
+        });
+    }
+
     private void modeSelect(int mode) {
         switch (mode) {
             case 1:
-                imgMode1.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
-                imgMode2.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                imgMode3.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                txtMode1.setTextColor(colorBlue);
-                txtMode2.setTextColor(Color.WHITE);
-                txtMode3.setTextColor(Color.WHITE);
-                groupMode1.setVisibility(View.VISIBLE);
-                groupMode2.setVisibility(View.INVISIBLE);
-                groupMode3.setVisibility(View.INVISIBLE);
+                mImgMode1.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
+                mImgMode2.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mImgMode3.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mTxtMode1.setTextColor(colorBlue);
+                mTxtMode2.setTextColor(Color.WHITE);
+                mTxtMode3.setTextColor(Color.WHITE);
+                mGroupMode1.setVisibility(View.VISIBLE);
+                mGroupMode2.setVisibility(View.INVISIBLE);
+                mGroupMode3.setVisibility(View.INVISIBLE);
                 break;
             case 2:
-                imgMode1.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                imgMode2.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
-                imgMode3.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                txtMode1.setTextColor(Color.WHITE);
-                txtMode2.setTextColor(colorBlue);
-                txtMode3.setTextColor(Color.WHITE);
-                groupMode1.setVisibility(View.INVISIBLE);
-                groupMode2.setVisibility(View.VISIBLE);
-                groupMode3.setVisibility(View.INVISIBLE);
+                mImgMode1.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mImgMode2.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
+                mImgMode3.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mTxtMode1.setTextColor(Color.WHITE);
+                mTxtMode2.setTextColor(colorBlue);
+                mTxtMode3.setTextColor(Color.WHITE);
+                mGroupMode1.setVisibility(View.INVISIBLE);
+                mGroupMode2.setVisibility(View.VISIBLE);
+                mGroupMode3.setVisibility(View.INVISIBLE);
                 break;
             default:
-                imgMode1.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                imgMode2.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
-                imgMode3.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
-                txtMode1.setTextColor(Color.WHITE);
-                txtMode2.setTextColor(Color.WHITE);
-                txtMode3.setTextColor(colorBlue);
-                groupMode1.setVisibility(View.INVISIBLE);
-                groupMode2.setVisibility(View.INVISIBLE);
-                groupMode3.setVisibility(View.VISIBLE);
+                mImgMode1.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mImgMode2.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                mImgMode3.setColorFilter(colorBlue, PorterDuff.Mode.SRC_ATOP);
+                mTxtMode1.setTextColor(Color.WHITE);
+                mTxtMode2.setTextColor(Color.WHITE);
+                mTxtMode3.setTextColor(colorBlue);
+                mGroupMode1.setVisibility(View.INVISIBLE);
+                mGroupMode2.setVisibility(View.INVISIBLE);
+                mGroupMode3.setVisibility(View.VISIBLE);
         }
     }
 }
+
