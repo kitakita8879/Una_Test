4e1324c35169ac303ac219cd66cd0700adcc911a
Una <unaliu@horizon-brands.com>
2024-05-07 17:59:39 +0800

    [Una Test 4] issue 1. 功能練習broadcast ，不會call onReceive()

--- app/src/main/java/com/example/una_test/TestComponentsActivity.java
+++ app/src/main/java/com/example/una_test/TestComponentsActivity.java
@@ -1,16 +1,28 @@
 package com.example.una_test;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TestComponentsActivity extends AppCompatActivity {
+    private Button btnBroadcast;
+    private IntentFilter mIntentFilter;
+
+    private static final String TEST_BROADCAST = "TEST_BROADCAST";
+    private static final String TEST_ACTION = "com.example.una_test.MyDynamicReceiver";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +32,36 @@ public class TestComponentsActivity extends AppCompatActivity {
         findViewById(R.id.btn_start).setOnClickListener(v -> startService(intent));
         findViewById(R.id.btn_stop).setOnClickListener(v -> stopService(intent));
 
-        findViewById(R.id.btn_bind).setOnClickListener(v -> bindService(intent, connection,
+        findViewById(R.id.btn_bind).setOnClickListener(v -> bindService(intent, mConnection,
                 BIND_AUTO_CREATE));
+        findViewById(R.id.btn_unbind).setOnClickListener(v -> unbindService(mConnection));
 
-        findViewById(R.id.btn_unbind).setOnClickListener(v -> unbindService(connection));
+        btnBroadcast = findViewById(R.id.btn_broadcast);
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(TEST_ACTION);
+
+        btnBroadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(TEST_ACTION);
+                intent.putExtra(TEST_BROADCAST, "Dynamic Broadcast data 8888");
+                sendBroadcast(intent);
+                Log.d("MyDynamicReceiver", "broadcast button onClick");
+            }
+        });
     }
 
-    ServiceConnection connection = new ServiceConnection() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(mDynamicReceiver, mIntentFilter, RECEIVER_NOT_EXPORTED);
+        }
+        Log.d("MyDynamicReceiver", "register Receiver");
+    }
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             MyService.MyBinder mBinder = (MyService.MyBinder) service;
@@ -37,6 +72,14 @@ public class TestComponentsActivity extends AppCompatActivity {
         @Override
         public void onServiceDisconnected(ComponentName name) {
             Log.d("MyService", "onServiceDisconnected");
+        }
+    };
+
+    private final BroadcastReceiver mDynamicReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String msg = intent.getStringExtra(TEST_BROADCAST);
+            Log.d("MyDynamicReceiver", "onReceive: " + msg);
         }
     };
 }
