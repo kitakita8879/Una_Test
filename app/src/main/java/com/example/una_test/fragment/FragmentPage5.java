a29d29cbfd868fd4d3321a0a569b61903efba98d
Una <unaliu@horizon-brands.com>
2024-05-16 12:18:06 +0800

    [Una Test 5] 功能練習 1. intent action練習

--- app/src/main/java/com/example/una_test/fragment/FragmentPage5.java
+++ app/src/main/java/com/example/una_test/fragment/FragmentPage5.java
@@ -1,15 +1,23 @@
 package com.example.una_test.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.una_test.R;
+
+import java.util.Objects;
 
 public class FragmentPage5 extends Fragment {
     @Nullable
@@ -17,5 +25,24 @@ public class FragmentPage5 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_page_5, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageView img = view.findViewById(R.id.img);
+        ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Uri selectedUri = Objects.requireNonNull(result.getData()).getData();
+                        img.setImageURI(selectedUri);
+                    }
+                });
+        view.findViewById(R.id.btn_image).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            resultLauncher.launch(intent);
+        });
     }
 }
