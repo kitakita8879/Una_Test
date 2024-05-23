2f68b3b42fe2052f76e91f211e197011fcd31449
Una <unaliu@horizon-brands.com>
2024-05-23 10:11:06 +0800

    [Una Test 7] 新增功能 1 SearchGraph深度搜尋DFS題目練習 2. searchGraphChoco() function 為 Choco 寫法改寫

--- app/src/main/java/com/example/una_test/TestThreadActivity.java
+++ app/src/main/java/com/example/una_test/TestThreadActivity.java
@@ -5,15 +5,28 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TestThreadActivity extends AppCompatActivity {
 
     private final String TAG = "THREAD_PRACTISE";
+    private TextView txtAns, txtAns2;
+    private long startTime;
+    private final HashSet<Integer> mNodeSet = new HashSet<>();
+    private final List<Integer> output = new ArrayList<>();
+    private final List<Integer> output2 = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +34,9 @@ public class TestThreadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_thread);
 
         TextView txtHandler = findViewById(R.id.txt_handler);
+        EditText editNode = findViewById(R.id.edit_node);
+        txtAns = findViewById(R.id.txt_ans);
+        txtAns2 = findViewById(R.id.txt_ans2);
 
         findViewById(R.id.btn_thread).setOnClickListener(v -> {
             new MyThread().start();
@@ -95,8 +111,134 @@ public class TestThreadActivity extends AppCompatActivity {
                 }.start();
             }
         });
+
+        findViewById(R.id.btn_input).setOnClickListener(v -> {
+            int start = checkNum(editNode);
+            if (start != 0) {
+                searchGraph(start);
+                searchGraphChoco(start);
+                startTime = System.currentTimeMillis();
+            }
+        });
     }
 
+    private int checkNum(EditText editNum) {
+        try {
+            int num = Integer.parseInt(editNum.getText().toString());
+            if (num == 0 || num > 7) {
+                throw new NumberFormatException();
+            }
+            return num;
+        } catch (NumberFormatException e) {
+            Log.d(TAG, e.toString());
+            txtAns.setText(R.string.label_please_input_17_sign);
+            txtAns2.setText(R.string.label_please_input_17_sign);
+            return 0;
+        }
+    }
+
+    @NonNull
+    private List<Integer> fetchNeighbours(int node) {
+        try {
+            // 模擬網路請求延遲
+            Thread.sleep((int) (Math.random() * 10) * 100L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        switch (node) {
+            case 1:
+                return Arrays.asList(2, 3, 4);
+            case 2:
+                return Arrays.asList(1, 5);
+            case 3:
+                return Arrays.asList(1, 5);
+            case 4:
+                return Arrays.asList(1, 6);
+            case 5:
+                return Arrays.asList(2, 3, 7);
+            case 6:
+                return Arrays.asList(4, 7);
+            case 7:
+                return Arrays.asList(5, 6);
+            default:
+                throw new IllegalArgumentException("node " + node + " not define");
+        }
+    }
+
+    private void searchGraph(int start) {
+        output.clear();
+        new Thread(() -> fetchShow(Collections.singletonList(start))).start();
+    }
+
+    private void fetchShow(List<Integer> fetchList) {
+        List<Integer> tmp = new ArrayList<>();
+        for (int j = 0; j < output.size(); j++) {
+            for (int k = fetchList.size() - 1; k >= 0; k--) {
+                if (output.get(j).equals(fetchList.get(k))) {
+                    fetchList.remove(k);
+                }
+            }
+        }
+        fetchList = fetchList.stream().distinct().collect(Collectors.toList());
+        Thread[] threads = new Thread[fetchList.size()];
+        for (int i = 0; i < fetchList.size(); i++) {
+            int index = fetchList.get(i);
+            output.add(index);
+            List<Integer> newOutput = new ArrayList<>(output);
+            runOnUiThread(() -> {
+                long currentTime = System.currentTimeMillis() - startTime;
+                txtAns.setText(String.format("%s %s ms", newOutput, currentTime));
+            });
+            threads[i] = new Thread(() -> tmp.addAll(fetchNeighbours(index)));
+            threads[i].start();
+        }
+        for (int i = 0; i < fetchList.size(); i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (!tmp.isEmpty()) {
+            fetchShow(tmp);
+        }
+    }
+
+    private void searchGraphChoco(int start) {
+        mNodeSet.clear();
+        output2.clear();
+        // 請在非 UI 線程執行
+        new Thread(() -> printAndFetch(Collections.singletonList(start))).start();
+        // 如果嚴格要求一行 print 所有內容就另外帶入 List, 把 Log 的 node 存入 List, 上面執行完再 print List
+    }
+
+    private void printAndFetch(@NonNull List<Integer> nodes) {
+        ArrayList<Integer> nextNodes = new ArrayList<>();
+        Thread[] threads = new Thread[nodes.size()];
+        for (Integer node : nodes) {
+            if (!mNodeSet.add(node)) {
+                continue;
+            }
+            output2.add(node);
+            List<Integer> newOutput = new ArrayList<>(output2);
+            runOnUiThread(() -> {
+                long currentTime = System.currentTimeMillis() - startTime;
+                txtAns2.setText(String.format("%s %s ms", newOutput, currentTime));
+            });
+            threads[nodes.indexOf(node)] = new Thread(() -> nextNodes.addAll(fetchNeighbours(node)));
+            threads[nodes.indexOf(node)].start();
+        }
+        for (int i = 0; i < nodes.size(); i++) {
+            try {
+                if (threads[i] != null)
+                    threads[i].join();
+            } catch (InterruptedException ignore) {
+            }
+        }
+        if (!nextNodes.isEmpty()) {
+            printAndFetch(nextNodes);
+        }
+    }
 
     private class MyRunnable implements Runnable {
 
